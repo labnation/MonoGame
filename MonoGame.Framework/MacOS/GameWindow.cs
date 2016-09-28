7549dcfd278e3ea0cbf288d62d1d98f33bc97c7d
Jasper van Bourgognie <van.bourgognie@gmail.com>
2016-09-28 15:43:49 +0200

    MacOS: GameWindow: Use Viewport constructor instead of struct default initialisation
    In order to have MaxDepth set to 1.0f by default (and have viewport.Unproject work properly), use a constructor ensuring the setting of MaxDepth

--- MonoGame.Framework/MacOS/GameWindow.cs
+++ MonoGame.Framework/MacOS/GameWindow.cs
@@ -204,17 +204,14 @@ namespace Microsoft.Xna.Framework
             if (_game.Initialized)
             {
     			manager.OnDeviceResetting(EventArgs.Empty);
-    			
-    			Microsoft.Xna.Framework.Graphics.Viewport _vp =
-    			new Microsoft.Xna.Framework.Graphics.Viewport();
+
+                Microsoft.Xna.Framework.Graphics.Viewport _vp =
+                new Microsoft.Xna.Framework.Graphics.Viewport (
+                     (int)Bounds.X, (int)Bounds.Y,
+                     (int)Bounds.Width, (int)Bounds.Height);
     			
     			_game.GraphicsDevice.PresentationParameters.BackBufferWidth = (int)Bounds.Width;
     			_game.GraphicsDevice.PresentationParameters.BackBufferHeight = (int)Bounds.Height;
-
-    			_vp.X = (int)Bounds.X;
-    			_vp.Y = (int)Bounds.Y;
-    			_vp.Width = (int)Bounds.Width;
-    			_vp.Height = (int)Bounds.Height;
 
     			_game.GraphicsDevice.Viewport = _vp;
             }
